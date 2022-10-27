efe2ae6585c1e08ec054d2d4a9ef9660dbfe2658
Vinicius Caldeira <vibarile@hotmail.com>
2022-10-26 23:16:40 -0300

    att css

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('#test-output').append(`Olá visitante, escolha uma das opções:\n
+    $('#test-output').append(`Olá visitante, estou aqui para ajudar vc na sua decisão do seu pedido. Escolha uma das opções abaixo:\n
 1 - DOCES
 2 - SALGADOS
 3 - VEGETARIANOS
